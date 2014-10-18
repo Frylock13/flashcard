14ee9260eab294a22de75e5f50ff9156e9ecd1ce
Vitaly <peter7875@gmail.com>
2014-10-19 01:07:45 +0400

    Methods into model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @card = Card.check_date.order("RANDOM()").first
+    @card = Card.cards_for_review.first
   end
 
   def check
-    @card = Card.find(params[:id])
-    if @card.translated_text == params[:translated_text]
+    @card = Card.find(params[:card_id])
+    if @card.check_answer(params[:translated_text])
       flash[:notice] = "Верно"
-      @card.update(review_date: DateTime.now + 3.days)
     else
       flash[:notice] = "Не верно"
     end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,17 @@
 class Card < ActiveRecord::Base
   validates :origin_text, :translated_text, presence: true
 
-  scope :check_date, -> { where('review_date <= ?', DateTime.now) }
+  scope :cards_for_review, -> { where('review_date <= ?', DateTime.now).order("RANDOM()") }
+
+  def check_answer(answer)
+    if answer == self.translated_text
+      change_review_date
+    else
+      false
+    end
+  end
+
+  def change_review_date
+    self.update(review_date: DateTime.now + 3.days)
+  end
 end

69a4b51c0a0c5006b519cd6236010bf575be403a
Vitaly <peter7875@gmail.com>
2014-10-20 21:35:13 +0400

    fix

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -7,12 +7,12 @@ describe Card do
     expect(@card.check_answer("яблоко")).to be true
     expect(@card.check_answer("Яблоко")).to be true
     expect(@card.review_date).to eq Date.today + 3.days
-    expect(@card.check_answer("2")).to be false
   end
 
   it "check the wrong answer and date" do
     @card = Card.new(origin_text: "apple", translated_text: "яблоко", review_date:DateTime.now.midnight)
     expect(@card.check_answer("вишня")).to be false
+    expect(@card.check_answer("2")).to be false
     expect(@card.review_date).to eq Date.today
   end
 

98750082c563e220c529e0f2cd236e95b5b5a3fa
Vitaly <peter7875@gmail.com>
2014-10-24 16:37:11 +0400

    delete field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
 def flash_class(type)
   case type
-    when "danger" then "alert-danger"
-    when "access" then "alert-success"
-    when "info" then "alert-info"
+    when "danger"  then "alert-danger"
+    when "access"  then "alert-success"
+    when "info"    then "alert-info"
     when "warning" then "alert-warning"
   end
 end

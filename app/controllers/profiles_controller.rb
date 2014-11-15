184ff11f3cd55f1605484a8e78fa54a1a0eacf68
Vitaly <Frylock13@users.noreply.github.com>
2014-11-15 03:08:31 +0300

    Revert "Revert "Profile""

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,33 @@
+class ProfilesController < ApplicationController
+  before_action :set_profile
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      flash[:access] = "Данные успешно обновлены"
+      redirect_to profile_path
+    else
+      flash[:danger] = "Произошла ошибка при обновлении данных"
+      render :edit
+    end
+  end
+
+  def reset_current_pack
+    @profile.update_attribute(:current_pack_id, nil)
+    redirect_to packs_path
+  end
+
+  private
+    def set_profile
+      @profile = current_user
+    end
+
+    def profile_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :current_pack_id, :image)
+    end
+  end

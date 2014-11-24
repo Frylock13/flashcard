490222a29820aaa9fec464d6daadccfc334b0acd
Vitaly <croaton6@gmail.com>
2014-11-24 05:40:30 +0400

    secrets.yml

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,7 +4,7 @@ class Card < ActiveRecord::Base
   has_attached_file :image, styles: { original: "360x360>", thumb: "40x40>" },
                             default_url: "/images/:style/missing.png",
                             storage: :s3,
-                            s3_credentials: "#{Rails.root}/config/aws.yml"
+                            s3_credentials: "#{Rails.root}/config/secrets.yml"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
 
